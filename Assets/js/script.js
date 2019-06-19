c12112342aad137321bd01abf57477936b85c200
Valentina <valentinavalenzuela.v@gmail.com>
2019-06-19 20:42:24 -0300

    my first commit

---
+++ Assets/js/script.js
@@ -0,0 +1,23 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+
+  $("a").on('click', function(event) {
+
+      if (this.hash !== "") {
+
+        event.preventDefault();
+
+
+        var hash = this.hash;
+
+
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+
+
+          window.location.hash = hash;
+        });
+      } 
+    });
+})
